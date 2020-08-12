eaf08792013afd8e0bcfde7da0348287f30794c0
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-08-12 22:10:25 +0600

    Add files via upload

---
+++ cse lab 5 task1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+   int i=0;
+   while(i<101)
+   {
+     if(i%2==0)
+    printf("%d\n",i);
+    i++;
+
+  }
+    printf("Done");
+
+ return 0;
+
+}
---
+++ cse lab 5 task3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,x, term=0,sum=0;
+    scanf("%d",&x);
+    for(i=1;i<=x;i++)
+    {
+
+
+            term=i;
+            sum=sum+term;
+
+    }
+
+        printf("The total sum is \n",sum);
+
+   return 0;
+
+}
+
