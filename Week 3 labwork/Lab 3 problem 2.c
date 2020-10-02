eb491311b94bb4c772733f7416f723276d079a35
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-10-02 20:10:00 +0600

    Add files via upload

---
+++ Week 3 labwork/Lab 3 problem 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int num;
+ printf("Enter the number=");
+ scanf("%d",&num);
+ if(num%7 ==0)
+  printf("number is multiple of 7");
+  else
+  printf("number is not multiple of 7");
+  return 0;
+
+}
