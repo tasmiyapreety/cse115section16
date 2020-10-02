8dbeec260fd4dbf00a6da9cac9829d8e9eb2da03
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-10-02 20:07:25 +0600

    Add files via upload

---
+++ Week 3 labwork/Lab 3 problem 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int x;
+ printf("Enter the value of x=");
+ scanf("%d", &x);
+ if(x>0)
+ printf(" x is positve");
+ else if(x<0)
+ printf("x is negative");
+return 0;
+}
