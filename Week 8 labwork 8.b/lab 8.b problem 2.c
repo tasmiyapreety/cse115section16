06b4af4737e6115ebb74f4ea397eb7ab37ef288d
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-10-03 00:08:20 +0600

    Add files via upload

---
+++ Week 8 labwork 8.b/lab 8.b problem 2.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int computeAverage(int a,int b)
+{
+    return(a+b)/2;
+}
+int main()
+{
+    int a,b, average;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("Enter b:");
+    scanf("%d",&b);
+
+    average= computeAverage(a,b);
+    printf("Average:%d",average);
+    return 0;
+}
