2ca3b35ede1519fd73f1a6a7de54c80673e5b439
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-10-02 19:33:39 +0600

    Add files via upload

---
+++ Week 10 labwork/Lab 10 problem 6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void printString(char s[]);
+int main()
+{
+ char str1[10];
+ gets(str1);
+ printString(str1);
+ return 0;
+}
---
+++ Week 10 labwork/Lab 10 prpblem 7.c
@@ -0,0 +1,10 @@
+
+void printString(char s[])
+{
+ int i=0;
+ while(s[i]!='\0')
+ {
+ printf("%c",s[i]);
+ i++;
+ }
+}
