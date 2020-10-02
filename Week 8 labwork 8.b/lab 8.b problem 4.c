c64a2c76c2c8c2546255308853c1ca4f57a1b8cd
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-10-03 00:13:03 +0600

    Add files via upload

---
+++ Week 8 labwork 8.b/lab 8.b problem 4.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+void printStarPyramid(int lines);
+int main()
+{
+    int lines;
+    printf("Enter lines:");
+    scanf("%d",&lines);
+
+    printStarPyramid(lines);
+    return 0;
+}
+void printStarPyramid(int lines)
+{
+    int i,j;
+    for(i=0;i<=lines;i++)
+    {
+        for(j=1;j<=lines-i;j++)
+            printf(" ");
+        for(j=1;j<=2*i-1;j++)
+            printf("*");
+        printf("\n");
+    }
+return 0;
+}
