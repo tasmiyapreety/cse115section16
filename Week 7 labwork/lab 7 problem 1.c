d350d91b1b5d53ec5ce187f7dd7a47583511cb46
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-10-02 23:54:50 +0600

    Add files via upload

---
+++ Week 7 labwork/lab 7 problem 1.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main()
+{
+  int a[7],n,i;
+  printf("Enter size of the array:");
+  scanf("%d",&n);
+  printf("Enter the element of the array:");
+  for (i=0;i<=n;i++)
+  {
+      scanf("%d",&a[1]);
+  }
+  printf("after reversing the array element are");
+  for(i=n-1;i>=0;i--)
+  {
+      printf("");
+      printf("%d",a[1]);
+  }
+
+ return 0;
+}
