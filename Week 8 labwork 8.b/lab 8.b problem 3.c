1076dff98fdb12fa94d29ccf01a673b5e785a479
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-10-03 00:10:47 +0600

    Add files via upload

---
+++ Week 8 labwork 8.b/lab 8.b problem 3.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+double getArea(int a,int b,int h)
+{
+    return((float)1/2)*(a+b)*h;
+}
+int main()
+{
+    int a,b,h;
+    double trapezoid;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("Enter b:");
+    scanf("%d",&b);
+    printf("Enter h:");
+    scanf("%d",&h);
+
+    trapezoid= getArea(a,b,h);
+    printf("%0.1f",trapezoid);
+    return 0;
+}
