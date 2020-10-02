4742572d15c7ad4a5aea10cac232065f2da68e4f
tasmiyapreety <68657392+tasmiyapreety@users.noreply.github.com>
2020-10-03 01:03:05 +0600

    Add files via upload

---
+++ Assignment 3/problem 4.c
@@ -0,0 +1,39 @@
+
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter the size of the array:\n");
+  scanf("%d",&n);
+
+  int arr[n],flag=0;
+  printf("Enter the elements of array:\n");
+
+  for(int i=0;i<n;i++)
+  {
+      printf("[%d]:",i);
+      scanf("%d",&arr[i]);
+  }
+
+   for(int i=0;i<n;i++)
+   {
+       for(int j=i+1;j<n;++j)
+       {
+           if(arr[i]==arr[j])
+           {
+               flag=1;
+               printf("Find %d at index %d %d\n",arr[i],i,j);
+           }
+       }
+   }
+
+   if(flag==0)
+   {
+       printf("There is no duplicate elements !");
+   }
+
+  return 0;
+
+
+}
+
